fe942f0ae6c867ff493cedf7157ebccdd1d2c15d
Fabian Beltran <fabianbrine@gmail.com>
2022-09-18 11:06:44 -0700

    added form handling

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,22 +1,42 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Container, Typography, TextField, Stack, Button } from '@mui/material'
+import { Container, Typography, TextField, Stack, Button, Box } from '@mui/material'
+import { useForm, Controller } from 'react-hook-form';
 
 const Login = () => {
+    const { handleSubmit, control, formState: { errors } } = useForm();
+
+    const onSubmit = (form) => {
+        console.log(form);
+    };
+
     return (
         <Container sx={{ py: 4 }} maxWidth='xs'>
             <Typography align='center' variant='h3' color='primary' sx={{ mb: 4 }}>
                 Login
             </Typography>
-            <Stack spacing={2}>
-                <TextField variant='outlined' label='Email' fullWidth/>
-                <TextField variant='outlined' label='Password' fullWidth/>
-                <Stack spacing={1} direction='row' justifyContent='center'>
-                    <Button variant='contained'>Login</Button>
-                    <Button component={Link} to='/register'>Register</Button>
+            <Box component='form' noValidate autoComplete='off' onSubmit={handleSubmit(onSubmit)}>
+                <Stack spacing={2}>
+                    {
+                        [
+                            { label: 'Email', formName: 'email', rules: { required: true } },
+                            { label: 'Password', formName: 'password', rules: { required: true } },
+                        ].map(({ label, formName, rules }) => <Box>
+                                <Controller
+                                    name={formName}
+                                    control={control}
+                                    rules={rules}
+                                    render={({ field }) => <TextField {...field} variant='outlined' label={label} required={rules.required} error={Boolean(errors[formName])} fullWidth/>}
+                                />
+                            </Box>
+                        )
+                    }
+                    <Stack spacing={1} direction='row' justifyContent='center'>
+                        <Button variant='contained' type='submit'>Login</Button>
+                        <Button component={Link} to='/register'>Register</Button>
+                    </Stack>
                 </Stack>
-            </Stack>
-            
+            </Box>
         </Container>
     );
 }
--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,20 +1,40 @@
 import React from 'react'
-import { Container, Typography, TextField, Stack, Button } from '@mui/material'
+import { Container, Typography, TextField, Stack, Button, Box } from '@mui/material';
+import { useForm, Controller } from 'react-hook-form'
 
 const Register = () => {
+    const { handleSubmit, control, formState: { errors } } = useForm();
+    
+    const onSubmit = (form) => {
+        console.log(form);
+    }
+
     return (
         <Container sx={{ py: 4 }} maxWidth='xs'>
             <Typography align='center' variant='h3' color='primary' sx={{ mb: 4 }}>
                 Register
             </Typography>
-            <Stack spacing={2}>
-                <TextField variant='outlined' label='First Name' required fullWidth/>
-                <TextField variant='outlined' label='Last Name' required fullWidth/>
-                <TextField variant='outlined' label='Email' required fullWidth/>
-                <TextField variant='outlined' label='Password' required fullWidth/>
-                <Button variant='contained'>Register</Button>
-            </Stack>
-            
+            <Box component='form' noValidate autoComplete='off' onSubmit={handleSubmit(onSubmit)}>
+                <Stack spacing={2}>
+                    {
+                        [
+                            { label: 'First Name', formName: 'firstName', rules: { required: true } },
+                            { label: 'Last Name', formName: 'lastName', rules: { required: true } },
+                            { label: 'Email', formName: 'email', rules: { required: true } },
+                            { label: 'Password', formName: 'password', rules: { required: true } },
+                        ].map(({ label, formName, rules }) => <Box>
+                                <Controller
+                                    name={formName}
+                                    control={control}
+                                    rules={rules}
+                                    render={({ field }) => <TextField {...field} variant='outlined' label={label} required={rules.required} error={Boolean(errors[formName])} fullWidth/>}
+                                />
+                            </Box>
+                        )
+                    }
+                    <Button variant='contained' type='submit'>Register</Button>
+                </Stack>
+            </Box>
         </Container>
     );
 }
