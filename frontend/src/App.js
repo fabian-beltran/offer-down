628d5ae8140e4a6d38d937b55e9544265270e346
Fabian Beltran <fabianbrine@gmail.com>
2022-09-18 11:22:32 -0700

    added private routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Appbar from './components/Appbar';
 import ThemeProvider from './components/ThemeProvider';
+import PrivateRoutes from './components/PrivateRoutes';
+
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
-
-// const router = createBrowserRouter([
-//     {
-//         path: '/',
-//         element: <Home />,
-//     },
-//     {
-//         path: '/login',
-//         element: <Login />,
-//     },
-//     {
-//         path: '/register',
-//         element: <Register />,
-//     },
-// ]);
+import AccountSettings from './pages/AccountSettings';
 
 function App() {
     return (
@@ -29,6 +17,10 @@ function App() {
                     <Route path='/' element={<Home />} />
                     <Route path='/login' element={<Login />} />
                     <Route path='/register' element={<Register />} />
+
+                    <Route element={<PrivateRoutes/>}>
+                        <Route element={<AccountSettings/>} path='/account-settings'/>
+                    </Route>
                 </Routes>
             </Router>
         </ThemeProvider>
